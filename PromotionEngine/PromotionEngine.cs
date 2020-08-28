b16007eb7956d0616ee13f860b4c40e53cf40f76
E60765_Cloudonly_OCP@orgcloudprod.onmicrosoft.com <EEM1KOR@BMHE1099149>
2020-08-28 17:10:48 +0530

    Promotion engine initial setup commit

---
+++ PromotionEngine/PromotionEngine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class PromotionEngine
+    {
+        int CalculateTotal()
+        {
+            return 0;
+        }
+    }
+}
