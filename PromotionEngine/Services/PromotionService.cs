166ab888c3f7d6836a46027cbadbfc26700c61c8
E60765_Cloudonly_OCP@orgcloudprod.onmicrosoft.com <EEM1KOR@BMHE1099149>
2020-08-28 17:20:43 +0530

    Promotion Service files added

---
+++ PromotionEngine/Services/PromotionService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Services
+{
+    public class PromotionService : IPromotionService
+    {
+
+    }
+}
