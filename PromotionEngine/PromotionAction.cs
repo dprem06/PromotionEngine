330e6a248632bd5e2421a867400b13160081088e
E60765_Cloudonly_OCP@orgcloudprod.onmicrosoft.com <EEM1KOR@BMHE1099149>
2020-08-28 17:32:51 +0530

    Promotion buisness action file added with calculate method

---
+++ PromotionEngine/PromotionAction.cs
@@ -0,0 +1,21 @@
+﻿using PromotionEngine.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class PromotionAction
+    {
+        private readonly IPromotionService _promotionService;
+        public PromotionAction(IPromotionService promotionService)
+        {
+            _promotionService = promotionService;
+        }
+
+        public int calculateTotal()
+        {
+            return 0;
+        }
+    }
+}
