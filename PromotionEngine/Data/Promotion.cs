636008a252a8e1a0a8648cb0923501381da56aa9
E60765_Cloudonly_OCP@orgcloudprod.onmicrosoft.com <EEM1KOR@BMHE1099149>
2020-08-28 17:16:08 +0530

    Promotion and sku data modal added

---
+++ PromotionEngine/Data/Promotion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Data
+{
+    public class Promotion
+    {
+        public int Id { get; set; }
+
+        public string PromotionCode { get; set; }
+
+        public List<PromotionSkuItem> SkuNames { get; set; }
+
+        public int DiscountPrice { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+    public class PromotionSkuItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Count { get; set; }
+    }
+}
---
+++ PromotionEngine/Data/SkuPriceList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Data
+{
+    public class SkuPriceList
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
