376335dd8a1579ade6f5a96f7cc99be05d783a3a
E60765_Cloudonly_OCP@orgcloudprod.onmicrosoft.com <EEM1KOR@BMHE1099149>
2020-08-28 17:35:12 +0530

    Cart data modal added

---
+++ PromotionEngine/Data/CartItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Data
+{
+    public class CartItems
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+
+    public class Cart
+    {
+        public List<CartItems> ListCartItems { get; set; }
+
+        public List<int> PromotionCodes { get; set; }
+
+        public int Total { get; set; }
+    }
+}
